1fc0e21668e78194a64888eb06be68a055f0026c
Ozan <oznkaraca@gmail.com>
2019-11-14 20:49:03 +0300

    add puppeteer and jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store, persistor } from "./Store";
-import { PersistGate } from "redux-persist/integration/react";
+import puppeteer from "puppeteer";
 
-it("renders without crashing", () => {
-  const app = (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  );
-  const div = document.createElement("div");
-  ReactDOM.render(app, div);
-  ReactDOM.unmountComponentAtNode(div);
+const APP = "http://localhost:3000/";
+
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+afterAll(() => {
+  browser.close();
 });
