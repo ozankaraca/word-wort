83c88ce4bb61ba0314dab38453064aaad8fbbe46
Ozan <oznkaraca@gmail.com>
2019-11-14 16:41:39 +0300

    Updates

---
+++ precache-manifest.8e20a91c9c1335b0afbd22d56257c79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2660b0bfbd0d8b1dd6be45118b220551",
+    "url": "/word-wort/index.html"
+  },
+  {
+    "revision": "d726ea759466f56cdca2",
+    "url": "/word-wort/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c3a031d6872ffe5f2092",
+    "url": "/word-wort/static/js/2.b0188963.chunk.js"
+  },
+  {
+    "revision": "d726ea759466f56cdca2",
+    "url": "/word-wort/static/js/main.97ad6795.chunk.js"
+  },
+  {
+    "revision": "ed20ed35434a81bea430",
+    "url": "/word-wort/static/js/runtime-main.7ff162df.js"
+  }
+]);
