b6cffa3e95db96476b41c77800edeb13af7ea421
Ozan <oznkaraca@gmail.com>
2019-11-14 21:51:00 +0300

    word bundle added to vocabulary list added

--- src/App.test.js
+++ src/App.test.js
@@ -19,3 +19,38 @@ beforeAll(async () => {
 afterAll(() => {
   browser.close();
 });
+
+describe("Add word to vocabulary list", () => {
+  test("word bundle added to vocabulary list", async () => {
+    await page.goto(APP);
+    await clickByText(page, "Expand Word List");
+    const rowCount = (await page.$$(".MuiListItem-container")).length;
+    await clickByText(page, "add");
+    await page.click("input[id=englishWordField]");
+    await page.type("input[id=englishWordField]", "Hello");
+    await page.click("input[id=germanWordField]");
+    await page.type("input[id=germanWordField]", "Hallo");
+    await clickByText(page, "ADD");
+    await clickByText(page, "BACK");
+    const rowCountAfterAdd = (await page.$$(".MuiListItem-container")).length;
+    expect(rowCountAfterAdd).toEqual(rowCount + 1);
+  }, 16000);
+});
+
+const escapeXpathString = str => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
+
+const clickByText = async (page, text) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(
+    `//span[contains(text(), ${escapedText})]`
+  );
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
